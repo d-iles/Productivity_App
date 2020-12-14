81de2a5083d75d2a9c61919baaaedf48a56cb653
d-iles <56329100+d-iles@users.noreply.github.com>
2020-12-14 11:43:58 -0600

    removed action bar

--- app/src/main/java/com/example/ourfirstapp11a/MainActivity.java
+++ app/src/main/java/com/example/ourfirstapp11a/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.ourfirstapp11a;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //ViewPager2 viewPager = (ViewPager2) findViewById(R.id.pager);
+        //viewPager.setOffscreenPageLimit(1);
+        CollectionDemoFragment cDF = new CollectionDemoFragment();
+        //SwipeAdapter swipeAdapter = new SwipeAdapter();
+        //viewPager.setAdapter(swipeAdapter);
+        //viewPager.setCurrentItem(0);
+
     }
 }
