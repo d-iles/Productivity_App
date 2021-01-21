5895b9ec2369e078a217e1c51e24fa09dbcb0510
d-iles <56329100+d-iles@users.noreply.github.com>
2021-01-20 19:53:06 -0600

    added date editor

--- app/src/main/java/com/example/ourfirstapp11a/ThirdFragment.java
+++ app/src/main/java/com/example/ourfirstapp11a/ThirdFragment.java
@@ -1,16 +1,27 @@
 package com.example.ourfirstapp11a;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import java.util.Calendar;
+
 public class ThirdFragment extends Fragment {
 
     private int pageNumber = 2;
+
+    TextView etDate;
+    DatePickerDialog.OnDateSetListener setListener;
 
     public ThirdFragment(int number) {
         pageNumber = number;
@@ -27,6 +38,33 @@ public class ThirdFragment extends Fragment {
         TextView text = view.findViewById(R.id.text1);
         int total = pageNumber + 1;
         text.setText("Page " + total);
+
+        etDate = view.findViewById(R.id.et_date);
+        Calendar calender = Calendar.getInstance();
+        final int year = calender.get(Calendar.YEAR);
+        final int month = calender.get(Calendar.MONTH);
+        final int day = calender.get(Calendar.DAY_OF_MONTH);
+
+        etDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePicker = new DatePickerDialog(ThirdFragment.this.getContext(),
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth, setListener, year, month, day);
+                datePicker.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePicker.show();
+            }
+        });
+
+        setListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month+1;
+                String date = month+"/"+dayOfMonth+"/"+year;
+                etDate.setText(date);
+            }
+        };
+
         return view;
     }
 }
