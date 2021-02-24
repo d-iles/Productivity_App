3de40931b2e4fe3df507573c1d9088b4f8f46b5a
d-iles <56329100+d-iles@users.noreply.github.com>
2021-02-24 17:58:49 -0600

    Added feature to remove focus from EditText - need to implement into and redo TimePicker class.

--- app/src/main/java/com/example/ourfirstapp11a/ThirdFragment.java
+++ app/src/main/java/com/example/ourfirstapp11a/ThirdFragment.java
@@ -8,10 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import java.util.Calendar;
@@ -20,7 +22,8 @@ public class ThirdFragment extends Fragment {
 
     private int pageNumber = 2;
 
-    TextView etDate;
+    TextView etDate; //
+    EditText editText1, editText2; // Title, desc
     DatePickerDialog.OnDateSetListener setListener;
 
     public ThirdFragment(int number) {
@@ -35,20 +38,22 @@ public class ThirdFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.adding_tasks, container, false );
-        TextView text = view.findViewById(R.id.text1);
-        int total = pageNumber + 1;
-        text.setText("Page " + total);
 
-        etDate = view.findViewById(R.id.et_date);
+        etDate = view.findViewById(R.id.et_date); // THis is the Date button
         Calendar calender = Calendar.getInstance();
         final int year = calender.get(Calendar.YEAR);
         final int month = calender.get(Calendar.MONTH);
         final int day = calender.get(Calendar.DAY_OF_MONTH);
 
+        editText1 = view.findViewById(R.id.editText1); // This is the Title
+        editText2 = view.findViewById(R.id.editText2); // This is the desc
+
         etDate.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+                editText1.clearFocus();
+                editText2.clearFocus();
                 DatePickerDialog datePicker = new DatePickerDialog(ThirdFragment.this.getContext(),
                         android.R.style.Theme_Holo_Light_Dialog_MinWidth, setListener, year, month, day);
                 datePicker.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
@@ -65,9 +70,33 @@ public class ThirdFragment extends Fragment {
             }
         };
 
+        // TODO Add TimePicker back into ThirdFragment, remove separate class
         TimePicker time = new TimePicker(view.findViewById(R.id.et_hour),ThirdFragment.this.getContext());
         time.setListener();
 
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideSoftKeyboard();
+                editText1.clearFocus();
+                editText2.clearFocus();
+            }
+        });
+
         return view;
     }
+
+    /* This method hides the keyboard */
+    public void hideSoftKeyboard() {
+        // TODO Add try-catch perhaps?
+        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(getActivity().INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = getActivity().getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(((Activity)this.getContext()));
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
 }
--- app/src/main/java/com/example/ourfirstapp11a/ViewPagerAdapter.java
+++ app/src/main/java/com/example/ourfirstapp11a/ViewPagerAdapter.java
@@ -3,6 +3,7 @@ package com.example.ourfirstapp11a;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 public class ViewPagerAdapter extends FragmentStateAdapter {
