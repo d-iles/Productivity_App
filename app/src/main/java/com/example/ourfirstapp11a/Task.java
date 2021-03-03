8d4b6afd7cae53237d9445bdaf14542f404dfa36
joegar000 <69176505+joegar000@users.noreply.github.com>
2021-03-03 17:30:40 -0600

    Added getter and setter methods to the Task.java class. Messed around with the layout of the recyclerview tasks, didn't do much though.

--- app/src/main/java/com/example/ourfirstapp11a/Task.java
+++ app/src/main/java/com/example/ourfirstapp11a/Task.java
@@ -1,8 +1,11 @@
 package com.example.ourfirstapp11a;
 
+import java.util.Calendar;
+
 public class Task {
     private int Importance;
     private String taskName;
+    private Calendar dueDate;
 
     /* Add a calculator for importance/worthiness in this later,
     maybe add a parameter(s) as a starting point for calculator */
@@ -12,5 +15,17 @@ public class Task {
 
     public String getName() {
         return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public Calendar getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Calendar dueDate) {
+        this.dueDate = dueDate;
     }
 }
