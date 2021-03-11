088eaf9e2afdf40944e9cc9d4111b775c82fd819
d-iles <56329100+d-iles@users.noreply.github.com>
2021-03-10 18:00:49 -0600

    Organized ThirdFragment code and added comments.

--- app/src/main/java/com/example/ourfirstapp11a/ThirdFragment.java
+++ app/src/main/java/com/example/ourfirstapp11a/ThirdFragment.java
@@ -2,6 +2,7 @@ package com.example.ourfirstapp11a;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -13,47 +14,61 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+
+/*
+    When adding new TextViews or objects to this code, remember that we have added a workaround to be
+    able to un-select textViews when clicking anywhere else on the screen. In order to apply the
+    workaround you must have an onclickListener for the object and add the clearFocus/hide-keyboard code.
+    Additionally, new TextViews must be added to the clearFocus method.
+ */
 
 public class ThirdFragment extends Fragment {
 
     private int pageNumber = 2;
 
-    TextView etDate; //
+    TextView dateButton; // This is the Date button
+    TextView timeButton; // This is the hour, minute button
     EditText editText1, editText2; // Title, desc
-    DatePickerDialog.OnDateSetListener setListener;
+    int tHour, tMinute; // Hour, minute for picking time
+    DatePickerDialog.OnDateSetListener setListener; // Used for picking Date
 
+    /** constructor for ThirdFragment */
     public ThirdFragment(int number) {
         pageNumber = number;
     }
 
+    /** onCreate occurs before onCreateView */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    /** onCreateView is called to have the fragment instantiate its user interface view. */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.adding_tasks, container, false );
 
-        etDate = view.findViewById(R.id.et_date); // THis is the Date button
+        dateButton = view.findViewById(R.id.et_date);
+        timeButton = view.findViewById(R.id.et_hour);
         Calendar calender = Calendar.getInstance();
         final int year = calender.get(Calendar.YEAR);
         final int month = calender.get(Calendar.MONTH);
         final int day = calender.get(Calendar.DAY_OF_MONTH);
 
-        editText1 = view.findViewById(R.id.editText1); // This is the Title
-        editText2 = view.findViewById(R.id.editText2); // This is the desc
+        editText1 = view.findViewById(R.id.editText1);
+        editText2 = view.findViewById(R.id.editText2);
 
-        etDate.setOnClickListener(new View.OnClickListener() {
-
+        // This onclick listener controls the dateButton behavior
+        dateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editText1.clearFocus();
-                editText2.clearFocus();
+                clearFocus();
                 DatePickerDialog datePicker = new DatePickerDialog(ThirdFragment.this.getContext(),
                         android.R.style.Theme_Holo_Light_Dialog_MinWidth, setListener, year, month, day);
                 datePicker.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
@@ -61,32 +76,60 @@ public class ThirdFragment extends Fragment {
             }
         });
 
+        // Sets the settings for the listener on the DatePickerDialog object
         setListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 month = month+1;
                 String date = month+"/"+dayOfMonth+"/"+year;
-                etDate.setText(date);
+                dateButton.setText(date);
             }
         };
 
-        // TODO Add TimePicker back into ThirdFragment, remove separate class
-        TimePicker time = new TimePicker(view.findViewById(R.id.et_hour),ThirdFragment.this.getContext());
-        time.setListener();
+        // This onclick listener controls the timeButton behavior
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideSoftKeyboard();
+                clearFocus();
+                TimePickerDialog timePickerDialog = new TimePickerDialog(
+                        ThirdFragment.this.getContext(),
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(android.widget.TimePicker view, int hourOfDay, int minute) {
+                                //Initialize hour and minute
+                                tHour = hourOfDay;
+                                tMinute = minute;
+                                String time = tHour + ":" + tMinute;
+                                SimpleDateFormat f24Hours = new SimpleDateFormat("HH:mm");
+                                try {
+                                    Date date = f24Hours.parse(time);
+                                    SimpleDateFormat f12Hours = new SimpleDateFormat("hh:mm aa");
+                                    timeButton.setText(f12Hours.format(date));
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, 12, 9, false
+                );
+                timePickerDialog.updateTime(tHour, tMinute);
+                timePickerDialog.show();
+            }
+        });
 
+        // onClick view, hide keyboard and clear focus from textViews
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 hideSoftKeyboard();
-                editText1.clearFocus();
-                editText2.clearFocus();
+                clearFocus();
             }
         });
 
         return view;
     }
 
-    /* This method hides the keyboard */
+    /** This method hides the keyboard */
     public void hideSoftKeyboard() {
         // TODO Add try-catch perhaps?
         InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(getActivity().INPUT_METHOD_SERVICE);
@@ -97,6 +140,12 @@ public class ThirdFragment extends Fragment {
             view = new View(((Activity)this.getContext()));
         }
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    /** This method clears the focus from all the TextViews */
+    public void clearFocus() {
+        editText1.clearFocus();
+        editText2.clearFocus();
     }
 
 }
